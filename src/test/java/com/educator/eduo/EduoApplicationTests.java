5ac67ca01ee1415485bb66ebdc5e52bac2728aa2
DaWoon Lee <qkgk61@gmail.com>
2022-12-07 13:57:03 +0900

    Init : Spring Boot 초기 설정

---
+++ src/test/java/com/educator/eduo/EduoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educator.eduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
