96ba0849827011e22a7562e90224fa971481b4dd
황준현 <ihrug000@gmail.com>
2022-12-08 21:11:49 +0900

    :art: refactor: userService login 기능 구조 리팩토링

--- src/main/java/com/educator/eduo/auth/model/service/UserService.java
+++ src/main/java/com/educator/eduo/auth/model/service/UserService.java
@@ -1,4 +1,8 @@
 package com.educator.eduo.auth.model.service;
 
+import com.educator.eduo.auth.model.dto.JwtResponse;
+import com.educator.eduo.auth.model.dto.LoginDto;
+
 public interface UserService {
+    JwtResponse authenticate(LoginDto loginDto);
 }
