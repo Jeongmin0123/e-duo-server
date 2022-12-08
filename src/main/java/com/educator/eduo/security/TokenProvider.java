7039257c3fed3b915c3ddb0a90de9e0db8ae51a1
DaWoon Lee <qkgk61@gmail.com>
2022-12-08 11:11:56 +0900

    Refactor : TokenProvider 클래스의 중복 코드 분리

--- src/main/java/com/educator/eduo/security/TokenProvider.java
+++ src/main/java/com/educator/eduo/security/TokenProvider.java
@@ -1,13 +1,6 @@
 package com.educator.eduo.security;
 
-/*
-* 토큰 생성, 조회, 유효성 검사
-*
-* 토큰 만들기
-* - Signature -> 서명 - SecretKey
-*
-* */
-
+import com.educator.eduo.auth.model.entity.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -20,7 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -31,107 +24,105 @@ import java.util.stream.Collectors;
 
 @Component
 public class TokenProvider implements InitializingBean {
-    private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
 
+    private static final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
     private static final String AUTHORITIES_KEY = "auth";
 
     private final String secret;
-    private final long accesstokenValidityInMilliSeconds;
-    private final long refreshtokenValidityInMilliSeconds;
-
+    private final long accessTokenValidityInMilliSeconds;
+    private final long refreshTokenValidityInMilliSeconds;
     private Key key;
 
     public TokenProvider(
             @Value("${jwt.secret}") String secret,
-            @Value("${jwt.accesstoken-validity-in-seconds}") long accesstokenValidityInMilliSeconds,
-            @Value("${jwt.refreshtoken-validity-in-seconds}") long refreshtokenValidityInMilliSeconds
+            @Value("${jwt.accesstoken-validity-in-seconds}") long accessTokenValidityInMilliSeconds,
+            @Value("${jwt.refreshtoken-validity-in-seconds}") long refreshTokenValidityInMilliSeconds
     ) {
         this.secret = secret;
-        this.accesstokenValidityInMilliSeconds = accesstokenValidityInMilliSeconds;
-        this.refreshtokenValidityInMilliSeconds = refreshtokenValidityInMilliSeconds;
+        this.accessTokenValidityInMilliSeconds = accessTokenValidityInMilliSeconds;
+        this.refreshTokenValidityInMilliSeconds = refreshTokenValidityInMilliSeconds;
     }
 
     @Override
-    public void afterPropertiesSet() throws Exception {
+    public void afterPropertiesSet() {
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    // 토큰 생성
     public String createAccessToken(Authentication authentication) {
-        // Authority
-//        authentication.getPrincipal() => User 객체로 담기도 한다.
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-        logger.info("Create Access Token ", authorities);
-
-        // 만료 시간
-        long now = (new Date()).getTime();
-        Date validity = new Date(now + this.accesstokenValidityInMilliSeconds);
-
-        // header.payload.signature
-        return Jwts.builder()
-                // Security 인증 인가  -> 접근주체 Principal = username==userId
-                .setSubject(authentication.getName()) // userId
-                .claim(AUTHORITIES_KEY, authorities) // auth: roles
-                .signWith(key, SignatureAlgorithm.HS512)
-                .setExpiration(validity)
-                .compact();
+        return createToken(authentication, this.accessTokenValidityInMilliSeconds);
     }
 
     public String createRefreshToken(Authentication authentication) {
-        // Authority
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-        System.out.println(authorities);
+        return createToken(authentication, this.refreshTokenValidityInMilliSeconds);
+    }
 
-        // set expiration time
-        long now = (new Date()).getTime();
-        Date validity = new Date(now + this.refreshtokenValidityInMilliSeconds);
+    public boolean validateToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            logger.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.info("만료된 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.info("잘못된 JWT 토큰입니다.");
+        }
 
-        return Jwts.builder()
-                .setSubject(authentication.getName()) // username
-                // claim에 key="auth" , data = username 을 넣기도 한다
-                .claim(AUTHORITIES_KEY, authorities) // Key, roles
-                .signWith(key, SignatureAlgorithm.HS512)
-                .setExpiration(validity)
-                .compact();
+        return false;
     }
 
     public Authentication getAuthentication(String token) {
-        Claims claims = Jwts
-                .parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = getClaims(token).getBody();
 
-        Collection<? extends GrantedAuthority> authorities =
-                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
+        UserDetails principal = User.builder()
+                                    .userId(claims.getSubject())
+                                    .build();
 
-        User principal = new User(claims.getSubject(), "", authorities);
-        // authentication 만들어 반환
+        Collection<? extends GrantedAuthority> authorities = getAuthorities(claims);
         return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 
-    // Token 유효성 검사
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
-        } catch(SecurityException | MalformedJwtException e) {
-            logger.info("잘못된 JWT 서명입니다.");
-        } catch(ExpiredJwtException e ) {
-            logger.info("만료된 토큰입니다.");
-        } catch(UnsupportedJwtException e) {
-            logger.info("지원되지 않는 JWT 토큰입니다.");
-        } catch(IllegalArgumentException e) {
-            logger.info("잘못된 JWT 토큰입니다.");
-        }
-        return false;
+    private String createToken(Authentication authentication, long validityTime) {
+        String authorities = getAuthorities(authentication);
+        logger.info("authorities of token: {}", authorities);
+
+        Date expirationTime = new Date(System.currentTimeMillis() + validityTime);
+        logger.info("expiration time of token: {}", expirationTime);
+
+        return Jwts.builder()
+                   .setSubject(authentication.getName())
+                   .claim(AUTHORITIES_KEY, authorities)
+                   .setExpiration(expirationTime)
+                   .signWith(this.key, SignatureAlgorithm.ES512)
+                   .compact();
     }
+
+    private String getAuthorities(Authentication authentication) {
+        return authentication.getAuthorities()
+                             .stream()
+                             .map(GrantedAuthority::getAuthority)
+                             .collect(Collectors.joining(","));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Claims claims) {
+        String[] authorities = claims.get(AUTHORITIES_KEY)
+                                     .toString()
+                                     .split(",");
+
+        return Arrays.stream(authorities)
+                     .map(SimpleGrantedAuthority::new)
+                     .collect(Collectors.toList());
+    }
+
+    private Jws<Claims> getClaims(String token)
+            throws SecurityException, MalformedJwtException, ExpiredJwtException, UnsupportedJwtException, IllegalArgumentException {
+        return Jwts.parserBuilder()
+                   .setSigningKey(this.key)
+                   .build()
+                   .parseClaimsJws(token);
+    }
+
 }
