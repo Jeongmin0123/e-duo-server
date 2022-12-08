a396ee0986c3051343fde45e719e7e1f5327af48
DaWoon Lee <qkgk61@gmail.com>
2022-12-08 11:52:15 +0900

    Style : 개행 조정

--- src/main/java/com/educator/eduo/security/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/educator/eduo/security/jwt/JwtAccessDeniedHandler.java
@@ -2,6 +2,7 @@ package com.educator.eduo.security.jwt;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -13,11 +14,13 @@ import java.io.IOException;
 
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
     private static final Logger logger = LoggerFactory.getLogger(JwtAccessDeniedHandler.class);
+
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response,
-                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         logger.error("Forbidden Error : {}", accessDeniedException.getMessage());
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "권한 실패");
+        response.sendError(HttpStatus.FORBIDDEN.value(), "권한, 인가 실패");
     }
+
 }
--- src/main/java/com/educator/eduo/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/educator/eduo/security/jwt/JwtAuthenticationEntryPoint.java
@@ -2,6 +2,7 @@ package com.educator.eduo.security.jwt;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -15,14 +16,13 @@ import java.io.IOException;
 // Authentication 관련 에러
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         logger.error("UnAuthorized Error : {}", authException.getMessage());
-
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증 실패");
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), "인증 실패");
     }
+
 }
