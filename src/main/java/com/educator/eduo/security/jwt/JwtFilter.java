1f0e6b8da8f71b6ece285cfe7ca53c639be50e77
DaWoon Lee <qkgk61@gmail.com>
2022-12-08 13:44:03 +0900

    Style : 변수명, 개행 조정

--- src/main/java/com/educator/eduo/security/jwt/JwtFilter.java
+++ src/main/java/com/educator/eduo/security/jwt/JwtFilter.java
@@ -18,40 +18,35 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+
     private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+    public static final String AUTHORIZATION_HEADER_KEY = "Authorization";
 
     private final TokenProvider tokenProvider;
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-//    headers: {
-//        "Authorization": "Bearer toeknsaljlkbdncldjfnd-0fknldsjbsd"
-//    }
 
-
-    // filterChain 내 순서가 됐을 때 이 로직을 수행하라
-    // 끝나면 filterChain.doFilter();
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         String jwt = resolveToken(request);
-        String requestURI = request.getRequestURI();
-        // 토큰이 존재 && 유효하다면
-        if(jwt != null && tokenProvider.validateToken(jwt)) {
-            logger.info("JwtFilet with Token : {}", jwt);
-            // Authentication 가져오기
-            Authentication authentication = tokenProvider.getAuthentication(jwt);
-            // Context에 authentication 저장
-            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            logger.debug("Security Context에 {} 인증 정보를 저장했습니다. uri: {}", authentication.getName(), requestURI);
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            logger.info("JwtFilter with Token : {}", jwt);
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            logger.debug("Security Context에 {} 인증 정보를 저장했습니다. URI: {}", authentication.getName(), request.getRequestURI());
         }
+
         filterChain.doFilter(request, response);
     }
 
-    // Header에서 토큰 꺼내오기
     private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER_KEY);
+
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7, bearerToken.length());
+            return bearerToken.substring(7);
         }
+
         return null;
     }
+
 }
