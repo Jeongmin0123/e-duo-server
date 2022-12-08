4ad3ddf703000ae4350b8fdd2dcd70acb7fe71ca
DaWoon Lee <qkgk61@gmail.com>
2022-12-08 10:17:19 +0900

    Feat : Spring Security Configuration 설정

--- src/main/java/com/educator/eduo/config/SecurityConfig.java
+++ src/main/java/com/educator/eduo/config/SecurityConfig.java
@@ -14,66 +14,54 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize, @PostAuthorize
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-
-//    public SecurityConfig(TokenProvider tokenProvider,
-//                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-//                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
-//        this.tokenProvider = tokenProvider;
-//        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-//        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers(HttpMethod.OPTIONS, "/**")
-                .antMatchers("/favicon.ico");
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.cors()
+                   .and()
+                   .csrf()
+                       .disable()
+                   .exceptionHandling()
+                       .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                       .accessDeniedHandler(jwtAccessDeniedHandler)
+                   .and()
+                   .sessionManagement()
+                       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                   .and()
+                   .authorizeRequests()
+                       .antMatchers("/api/**").authenticated()
+                       .antMatchers("/auth/**").permitAll()
+                       .anyRequest().authenticated()
+                   .and()
+                   .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+                   .build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .csrf().disable()
-
-                // exceptionHandler 등록
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler)
-
-
-                // Session Stateless
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/**").authenticated()
-                .antMatchers("/auth/**").permitAll() // /auth/login ->
-                .anyRequest().authenticated()
-
-                .and()
-                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                         .antMatchers(HttpMethod.OPTIONS, "/**")
+                         .antMatchers("/favicon.ico");
     }
 
 }
