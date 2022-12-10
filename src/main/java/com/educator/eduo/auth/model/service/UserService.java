6bfc0e826e9ca87699012a34ec8bf7152485b0cd
DaWoon Lee <qkgk61@gmail.com>
2022-12-10 21:34:39 +0900

    Refactor : 카카오 소셜 로그인 리팩토링

--- src/main/java/com/educator/eduo/auth/model/service/UserService.java
+++ src/main/java/com/educator/eduo/auth/model/service/UserService.java
@@ -2,11 +2,12 @@ package com.educator.eduo.auth.model.service;
 
 import com.educator.eduo.auth.model.dto.JwtResponse;
 import com.educator.eduo.auth.model.dto.LoginDto;
-import com.educator.eduo.auth.model.dto.OAuthLoginDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface UserService {
+
     JwtResponse authenticate(LoginDto loginDto);
 
+    JwtResponse getUserInfoUsingKakao(String accessTokenByKakao) throws JsonProcessingException;
 
-    JwtResponse oauthLogin(OAuthLoginDto oAuthLoginDto);
 }
--- src/main/java/com/educator/eduo/auth/model/service/UserServiceImpl.java
+++ src/main/java/com/educator/eduo/auth/model/service/UserServiceImpl.java
@@ -2,34 +2,61 @@ package com.educator.eduo.auth.model.service;
 
 import com.educator.eduo.auth.model.dto.JwtResponse;
 import com.educator.eduo.auth.model.dto.LoginDto;
-import com.educator.eduo.auth.model.dto.OAuthLoginDto;
 import com.educator.eduo.auth.model.entity.Token;
-import com.educator.eduo.auth.model.entity.User;
 import com.educator.eduo.auth.model.mapper.TokenMapper;
 import com.educator.eduo.auth.model.mapper.UserMapper;
 import com.educator.eduo.security.TokenProvider;
-import lombok.RequiredArgsConstructor;
+import com.educator.eduo.util.HttpUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-@RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+
     private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
     private final UserMapper userMapper;
     private final TokenMapper tokenMapper;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final TokenProvider tokenProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final String requestUrlForKakao;
 
+    @Autowired
+    public UserServiceImpl(
+            UserMapper userMapper,
+            TokenMapper tokenMapper,
+            AuthenticationManagerBuilder authenticationManagerBuilder,
+            TokenProvider tokenProvider,
+            PasswordEncoder passwordEncoder,
+            @Value("${kakao.social.url}") String requestUrlForKakao
+    ) {
+        this.userMapper = userMapper;
+        this.tokenMapper = tokenMapper;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.tokenProvider = tokenProvider;
+        this.passwordEncoder = passwordEncoder;
+        this.requestUrlForKakao = requestUrlForKakao;
+    }
 
     @Override
     @Transactional
@@ -42,16 +69,21 @@ public class UserServiceImpl implements UserService{
 
     @Override
     @Transactional
-    public JwtResponse oauthLogin(OAuthLoginDto oAuthLoginDto) {
-        User user = userMapper.selectUserByUserId(oAuthLoginDto.getUserId()).orElse(null);
-        if(user != null) {
-            Authentication authentication = new UsernamePasswordAuthenticationToken(
-                    oAuthLoginDto.getUserId(), "", user.getAuthorities()
-            );
-            Token newToken = registerOrUpdateJwtToken(authentication);
-            return tokenProvider.createJwtResponse(authentication, newToken);
-        }
-        return null;
+    public JwtResponse getUserInfoUsingKakao(String accessTokenByKakao) throws JsonProcessingException {
+        HttpHeaders headers = HttpUtil.generateHttpHeadersForJWT(accessTokenByKakao);
+        RestTemplate restTemplate = HttpUtil.generateRestTemplate();
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(requestUrlForKakao, HttpMethod.GET, request, String.class);
+
+        JsonNode json = new ObjectMapper().readTree(response.getBody());
+        String userId = json.get("kakao_accout").get("email").asText();
+        String pseudoPassword = UUID.randomUUID().toString();
+        LoginDto loginDto = LoginDto.builder()
+                                    .userId(userId)
+                                    .password(passwordEncoder.encode(pseudoPassword))
+                                    .build();
+        return authenticate(loginDto);
     }
 
     private Authentication saveAuthentication(LoginDto loginDto) {
