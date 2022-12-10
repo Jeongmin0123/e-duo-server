f03021fc97e94a0244c3a6aaad449e57e5d4b0a8
DaWoon Lee <qkgk61@gmail.com>
2022-12-11 02:54:46 +0900

    Feat : 메일 인증을 위한 메일 전송 기능 구현

---
+++ src/main/java/com/educator/eduo/auth/model/dto/AuthMailDto.java
@@ -0,0 +1,34 @@
+package com.educator.eduo.auth.model.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class AuthMailDto {
+
+    private final String from;
+    private final String to;
+    private final String subject;
+    private String content;
+
+    @Builder
+    public AuthMailDto(String from, String to, String subject) {
+        this.from = from;
+        this.to = to;
+        this.subject = subject;
+    }
+
+    public AuthMailDto setContent(String emailAuthCode) {
+        this.content = new StringBuilder()
+                .append("<div style='margin:100px;'>")
+                .append("<div align='center' style='border:1px solid black; font-family:verdana';>")
+                .append("<h3 style='color:blue;'>이메일 인증 코드입니다.</h3>")
+                .append("<div style='font-size:130%'>")
+                .append("CODE : <strong>").append(emailAuthCode).append("</strong><div><br/> ")
+                .append("</div>")
+                .toString();
+
+        return this;
+    }
+
+}
