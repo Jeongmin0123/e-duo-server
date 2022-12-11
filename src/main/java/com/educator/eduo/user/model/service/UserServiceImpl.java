5ad479841c68107705f3ab9b66d96227140eabe9
DaWoon Lee <qkgk61@gmail.com>
2022-12-12 03:17:02 +0900

    Refactor : UserService의 메서드 반환형 변경
    기존 방식은 0 또는 1을 반환하였지만 실제 사용 환경에서는 참, 거짓의 형태로 사용되므로 int에서 boolean으로 반환형 변경

--- src/main/java/com/educator/eduo/user/model/service/UserServiceImpl.java
+++ src/main/java/com/educator/eduo/user/model/service/UserServiceImpl.java
@@ -5,15 +5,18 @@ import com.educator.eduo.user.model.entity.Student;
 import com.educator.eduo.user.model.entity.Teacher;
 import com.educator.eduo.user.model.entity.User;
 import com.educator.eduo.user.model.mapper.UserMapper;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+
     private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
     private final UserMapper userMapper;
@@ -27,7 +30,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     @Transactional
-    public int updateUser(User user) {
+    public boolean updateUser(User user) {
         User searchResult = userMapper.selectUserByUserId(user.getUserId()).orElse(null);
 
         if(searchResult == null) return 0;
@@ -37,26 +40,26 @@ public class UserServiceImpl implements UserService{
 
     @Override
     @Transactional
-    public int updateTeacher(Teacher teacher) {
-        teacher.setPassword(passwordEncoder.encode(teacher.getPassword()));
+    public boolean updateTeacher(Teacher teacher) {
+        teacher.encryptPassword(passwordEncoder);
         userMapper.updateUser(teacher);
-        return userMapper.updateTeacher(teacher);
+        return userMapper.updateTeacher(teacher) == 1;
     }
 
     @Override
     @Transactional
-    public int updateAssistant(Assistant assistant) {
-        assistant.setPassword(passwordEncoder.encode(assistant.getPassword()));
+    public boolean updateAssistant(Assistant assistant) {
+        assistant.encryptPassword(passwordEncoder);
         userMapper.updateUser(assistant);
-        return userMapper.updateAssistant(assistant);
+        return userMapper.updateAssistant(assistant) == 1;
     }
 
     @Override
     @Transactional
-    public int updateStudent(Student student) {
-        student.setPassword(passwordEncoder.encode(student.getPassword()));
+    public boolean updateStudent(Student student) {
+        student.encryptPassword(passwordEncoder);
         userMapper.updateUser(student);
-        return userMapper.updateStudent(student);
+        return userMapper.updateStudent(student) == 1;
     }
 
 }
