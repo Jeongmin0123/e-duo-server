59b21322b4784fe49fba64b29164f26babe14aa1
황준현 <ihrug000@gmail.com>
2022-12-15 17:12:33 +0900

    :card_file_box:	fix: db 갈아엎기 힘들다

--- db/dummy_data.sql
+++ db/dummy_data.sql
@@ -17,48 +17,48 @@ INSERT INTO `e_duo`.`student` (`user_id`, `birth_date`, `school_name`, `grade`, 
 
 
 -- course
-INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('sadbiu1l2kjd51dwqp31', 'tlaxh000@naver.com', 'Spring Boot', 'SSAFY', '역삼동', '2022-07-06', '2023-06-25', '싸피에서 진행하는 Spring Boot Class');
-INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw', 'tlaxh000@naver.com', 'Vue 2.0', 'Multicampus', '대치동', '2022-11-01', '2023-08-15', '영차님이 진행하는 Vue 수업');
-INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('tokenjdnbudolndsad-bfj1', 'teacher@eduo.com', 'Network', 'NICE', '강남동', '2022-09-25', '2023-01-15', '호준이형이 가르치는 네트워크');
-INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('yysndbzgql-subdqb-zbvkq', 'teacher@eduo.com', 'DB', 'NICE', '강남동', '2022-09-25', '2023-01-15', '정민이가 때려박아주는 DB');
+INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('Mdk1-tla-5as1', 'tlaxh000@naver.com', 'Spring Boot', 'SSAFY', '역삼동', '2022-07-06', '2023-06-25', '싸피에서 진행하는 Spring Boot Class');
+INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('EOs5-teac-s86z', 'tlaxh000@naver.com', 'Vue 2.0', 'Multicampus', '대치동', '2022-11-01', '2023-08-15', '영차님이 진행하는 Vue 수업');
+INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('KsZ1-teac-9Zw1', 'teacher@eduo.com', 'Network', 'NICE', '강남동', '2022-09-25', '2023-01-15', '호준이형이 가르치는 네트워크');
+INSERT INTO `e_duo`.`course` (`course_id`, `user_id`, `course_name`, `academy_name`, `academy_address`, `start_date`, `end_date`, `description`) VALUES ('MOZM-newt-9xAz', 'teacher@eduo.com', 'DB', 'NICE', '강남동', '2022-09-25', '2023-01-15', '정민이가 때려박아주는 DB');
 
 -- schedule
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('sadbiu1l2kjd51dwqp31', '1', '09:00', '18:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('sadbiu1l2kjd51dwqp31', '3', '10:00', '22:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('sadbiu1l2kjd51dwqp31', '5', '15:00', '21:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw', '2', '12:00', '14:30');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw', '4', '18:00', '22:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('tokenjdnbudolndsad-bfj1', '1', '20:00', '23:30');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('tokenjdnbudolndsad-bfj1', '4', '19:00', '22:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('yysndbzgql-subdqb-zbvkq', '1', '09:00', '18:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('yysndbzgql-subdqb-zbvkq', '2', '09:00', '18:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('yysndbzgql-subdqb-zbvkq', '4', '09:00', '18:00');
-INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('yysndbzgql-subdqb-zbvkq', '5', '09:00', '18:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('Mdk1-tla-5as1', '1', '09:00', '18:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('Mdk1-tla-5as1', '3', '10:00', '22:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('Mdk1-tla-5as1', '5', '15:00', '21:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('EOs5-teac-s86z', '2', '12:00', '14:30');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('EOs5-teac-s86z', '4', '18:00', '22:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('KsZ1-teac-9Zw1', '1', '20:00', '23:30');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('KsZ1-teac-9Zw1', '4', '19:00', '22:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('MOZM-newt-9xAz', '1', '09:00', '18:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('MOZM-newt-9xAz', '2', '09:00', '18:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('MOZM-newt-9xAz', '4', '09:00', '18:00');
+INSERT INTO `e_duo`.`schedule` (`course_id`, `day`, `start_time`, `end_time`) VALUES ('MOZM-newt-9xAz', '5', '09:00', '18:00');
 
 
 -- lecture
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('sadbiu1l2kjd51dwqp31-1-1', 'sadbiu1l2kjd51dwqp31', '1주차 1차 : Spring Boot', '2022-11-01 00:00:00', '09:00', '18:00', '개요', '1-1');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('sadbiu1l2kjd51dwqp31-1-2', 'sadbiu1l2kjd51dwqp31', '1주차 2차 : Spring Boot', '2022-11-03 00:00:00', '10:00', '22:00', '스캠', '1-2');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('sadbiu1l2kjd51dwqp31-1-3', 'sadbiu1l2kjd51dwqp31', '1주차 3차 : Spring Boot', '2022-11-05 00:00:00', '15:00', '21:00', '1주차', '1-3');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('sadbiu1l2kjd51dwqp31-2-1', 'sadbiu1l2kjd51dwqp31', '2주차 1차 : Spring Boot', '2022-11-08 00:00:00', '09:00', '18:00', '몰라', '2-1');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('sadbiu1l2kjd51dwqp31-2-2', 'sadbiu1l2kjd51dwqp31', '2주차 2차 : Spring Boot', '2022-11-10 00:00:00', '10:00', '22:00', '뭔데', '2-2');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('sadbiu1l2kjd51dwqp31-2-3', 'sadbiu1l2kjd51dwqp31', '2주차 3차 : Spring Boot', '2022-11-12 00:00:00', '15:00', '21:00', '', '2-3');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw1-1', 'iqwybdjkwbid0-wqkdbqwjdqw', '1주차 1차 : Vue 2.0', '2022-07-06 00:00:00', '12:00', '14:30', '개요', '1-1');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw1-2', 'iqwybdjkwbid0-wqkdbqwjdqw', '1주차 2차 : Vue 2.0', '2022-07-08 00:00:00', '18:00', '22:00', '흐어', '1-2');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw-16-1', 'iqwybdjkwbid0-wqkdbqwjdqw', '16주차 1차 : Vue 2.0', '2022-11-04 00:00:00', '12:00', '14:30', '뷰', '16-1');
-INSERT INTO `e_duo`.`lecture` (`lecture_id`, `course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`, `lecture_order`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw16-2', 'iqwybdjkwbid0-wqkdbqwjdqw', '16주차 2차 : Vue 2.0', '2022-11-06 00:00:00', '18:00', '22:00', '알고싶어?', '16-2');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('Mdk1-tla-5as1', '1주차 1차 : Spring Boot', '2022-11-01 00:00:00', '09:00', '18:00', '개요');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('Mdk1-tla-5as1', '1주차 2차 : Spring Boot', '2022-11-03 00:00:00', '10:00', '22:00', '스캠');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('Mdk1-tla-5as1', '1주차 3차 : Spring Boot', '2022-11-05 00:00:00', '15:00', '21:00', '1주차');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('Mdk1-tla-5as1', '2주차 1차 : Spring Boot', '2022-11-08 00:00:00', '09:00', '18:00', '몰라');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('Mdk1-tla-5as1', '2주차 2차 : Spring Boot', '2022-11-10 00:00:00', '10:00', '22:00', '뭔데');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('Mdk1-tla-5as1', '2주차 3차 : Spring Boot', '2022-11-12 00:00:00', '15:00', '21:00', '');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('EOs5-teac-s86z', '1주차 1차 : Vue 2.0', '2022-07-06 00:00:00', '12:00', '14:30', '개요');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('EOs5-teac-s86z', '1주차 2차 : Vue 2.0', '2022-07-08 00:00:00', '18:00', '22:00', '흐어');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('EOs5-teac-s86z', '16주차 1차 : Vue 2.0', '2022-11-04 00:00:00', '12:00', '14:30', '뷰');
+INSERT INTO `e_duo`.`lecture` (`course_id`, `lecture_name`, `lecture_date`, `start_time`, `end_time`, `description`) VALUES ('EOs5-teac-s86z', '16주차 2차 : Vue 2.0', '2022-11-06 00:00:00', '18:00', '22:00', '알고싶어?');
 
 
 -- management
-INSERT INTO `e_duo`.`management`(`course_id`, `user_id`) VALUES ('iqwybdjkwbid0-wqkdbqwjdqw', 'chaa@eduo.com');
-INSERT INTO `e_duo`.`management`(`course_id`, `user_id`) VALUES ('sadbiu1l2kjd51dwqp31', 'chaa@eduo.com');
-INSERT INTO `e_duo`.`management`(`course_id`, `user_id`) VALUES ('sadbiu1l2kjd51dwqp31', 'man@eduo.com');
+INSERT INTO `e_duo`.`management`(`course_id`, `user_id`) VALUES ('EOs5-teac-s86z', 'chaa@eduo.com');
+INSERT INTO `e_duo`.`management`(`course_id`, `user_id`) VALUES ('Mdk1-tla-5as1', 'chaa@eduo.com');
+INSERT INTO `e_duo`.`management`(`course_id`, `user_id`) VALUES ('Mdk1-tla-5as1', 'man@eduo.com');
 
 -- sugang
-INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('one@naver.com', 'tokenjdnbudolndsad-bfj1', '2022-09-25', 'ACCEPTED');
-INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('one@naver.com', 'iqwybdjkwbid0-wqkdbqwjdqw', '2022-11-15', 'ACCEPTED');
-INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('three@naver.com', 'yysndbzgql-subdqb-zbvkq', '2022-09-25', 'ACCEPTED');
-INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('three@naver.com', 'iqwybdjkwbid0-wqkdbqwjdqw', '2022-11-05', 'ACCEPTED');
-INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('two@naver.com', 'iqwybdjkwbid0-wqkdbqwjdqw', '2022-12-15', 'ACCEPTED');
-INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('two@naver.com', 'sadbiu1l2kjd51dwqp31', '2022-08-15', 'ACCEPTED');
-INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('two@naver.com', 'tokenjdnbudolndsad-bfj1', '2022-09-25', 'ACCEPTED');
+INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('one@naver.com', 'KsZ1-teac-9Zw1', '2022-09-25', 'ACCEPTED');
+INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('one@naver.com', 'EOs5-teac-s86z', '2022-11-15', 'ACCEPTED');
+INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('three@naver.com', 'MOZM-newt-9xAz', '2022-09-25', 'ACCEPTED');
+INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('three@naver.com', 'EOs5-teac-s86z', '2022-11-05', 'ACCEPTED');
+INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('two@naver.com', 'EOs5-teac-s86z', '2022-12-15', 'ACCEPTED');
+INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('two@naver.com', 'Mdk1-tla-5as1', '2022-08-15', 'ACCEPTED');
+INSERT INTO `e_duo`.`sugang` (`user_id`, `course_id`, `start_date`, `state`) VALUES ('two@naver.com', 'KsZ1-teac-9Zw1', '2022-09-25', 'ACCEPTED');
